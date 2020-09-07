407656867afe7c8639d7ddbbe6e99ebbc213d1fc
Haw Loeung <haw.loeung@canonical.com>
2020-09-07 16:46:01 +1000

    Fixed to run nginx daemon

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,9 +4,12 @@ set -eu
 
 # https://pempek.net/articles/2013/07/08/bash-sh-as-template-engine/
 render_template() {
-  eval "echo \"$(cat $1)\""
+    eval "echo \"$(cat $1)\""
 }
 
 cp /srv/content-cache/files/nginx-logging-format.conf /etc/nginx/conf.d/nginx-logging-format.conf
 
 render_template /srv/content-cache/templates/nginx_cfg.tmpl > /etc/nginx/sites-available/default
+
+# Run the real command
+exec "$@"
