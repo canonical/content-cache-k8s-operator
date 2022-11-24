edef9759a86bbc139aed6c3e4909bc516c5a5096
gtrkiller <54121436+gtrkiller@users.noreply.github.com>
2022-11-24 10:51:00 -0300

    add cache_all option (#4)
    * resuming launchpad MP
    
    * fixing cache_all obtaining
    
    * address comments

--- src/charm.py
+++ src/charm.py
@@ -141,7 +141,7 @@ class ContentCacheCharm(CharmBase):
 
     def _generate_keys_zone(self, name):
         """Generate hashed name to be used by Nginx's key zone."""
-        return "{}-cache".format(hashlib.md5(name.encode("UTF-8")).hexdigest()[0:12])
+        return f"{hashlib.md5(name.encode('UTF-8')).hexdigest()[0:12]}-cache"
 
     def _make_ingress_config(self) -> list:
         """Return an assembled K8s ingress."""
@@ -211,6 +211,10 @@ class ContentCacheCharm(CharmBase):
                 backend_site_name = urlparse(backend).hostname
             site = config["site"]
 
+        cache_all_configs = ''
+        if not config['cache_all']:
+            cache_all_configs = "proxy_ignore_headers Cache-Control Expires;"
+
         client_max_body_size = config["client_max_body_size"]
 
         env_config = {
@@ -225,6 +229,7 @@ class ContentCacheCharm(CharmBase):
             # to pass to the pebble services and ensure it restarts
             # nginx on changes.
             "NGINX_BACKEND": backend,
+            "NGINX_CACHE_ALL": cache_all_configs,
             "NGINX_BACKEND_SITE_NAME": backend_site_name,
             "NGINX_CACHE_INACTIVE_TIME": config.get("cache_inactive_time", "10m"),
             "NGINX_CACHE_MAX_SIZE": config.get("cache_max_size", "10G"),
