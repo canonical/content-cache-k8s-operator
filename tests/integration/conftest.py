84731a96d530eda4fe354368dcf7c0286a071079
Arturo Seijas <arturo.seijas@canonical.com>
2024-03-13 11:48:06 +0100

    update loki lib only

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -104,6 +104,11 @@ async def nginx_integrator_app(ops_test: OpsTest):
     """Deploy nginx-ingress-integrator charm."""
     nginx_integrator_app_name = "nginx-ingress-integrator"
     nginx_integrator_app = await ops_test.model.deploy(nginx_integrator_app_name, trust=True)
+    await ops_test.model.wait_for_idle()
+    assert (
+        ops_test.model.applications[nginx_integrator_app_name].units[0].workload_status
+        == ActiveStatus.name
+    )
     yield nginx_integrator_app
 
 
