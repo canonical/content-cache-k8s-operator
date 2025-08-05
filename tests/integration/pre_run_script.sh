997ce3d13359f909e506079a0b6a04d3e9884037
Sebastien Georget <sebastien.georget@canonical.com>
2025-08-05 17:37:59 +0200

    Add missing file

---
+++ tests/integration/pre_run_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+# Pre-run script for integration test operator-workflows action.
+# https://github.com/canonical/operator-workflows/blob/main/.github/workflows/integration_test.yaml
+
+IPADDR=$(ip -4 -j route get 2.2.2.2 | jq -r '.[] | .prefsrc')
+sudo microk8s enable "metallb:$IPADDR-$IPADDR"
