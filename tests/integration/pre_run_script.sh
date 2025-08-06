5aa15925af264af129aaccfa46142ecc300a5168
Sebastien Georget <sebastien.georget@canonical.com>
2025-08-06 08:13:23 +0200

    Disable metallb

--- tests/integration/pre_run_script.sh
+++ tests/integration/pre_run_script.sh
@@ -6,5 +6,5 @@
 # Pre-run script for integration test operator-workflows action.
 # https://github.com/canonical/operator-workflows/blob/main/.github/workflows/integration_test.yaml
 
-IPADDR=$(ip -4 -j route get 2.2.2.2 | jq -r '.[] | .prefsrc')
-sudo microk8s enable "metallb:$IPADDR-$IPADDR"
+#IPADDR=$(ip -4 -j route get 2.2.2.2 | jq -r '.[] | .prefsrc')
+#sudo microk8s enable "metallb:$IPADDR-$IPADDR"
