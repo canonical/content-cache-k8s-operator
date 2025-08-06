1db0836c81dc2224023ea3da50a7733f493a8e03
Sebastien Georget <sebastien.georget@canonical.com>
2025-08-07 00:23:36 +0200

    Adapt to work when bug will be resolved

--- tests/integration/pre_run_script.sh
+++ tests/integration/pre_run_script.sh
@@ -6,9 +6,16 @@
 # Pre-run script for integration test operator-workflows action.
 # https://github.com/canonical/operator-workflows/blob/main/.github/workflows/integration_test.yaml
 
-# Workaround around a bug in Calico and/or MicroK8s which results in doubled
+# Workaround for a bug in Calico and/or MicroK8s which results in doubled
 # iptables entries to redirect traffic to k8s pods.
+#
 # Affected rule looks like:
 # num  target     prot opt source               destination
 #1    CNI-DN-6832ce82caf043689e15f  tcp  --  anywhere             anywhere             /* dnat name: "k8s-pod-network" id: "9b452011f0c5049380e9bc4ea23014fb984b8f8e20415f8f9e0c8e1ce5e26ac9" */ multiport dports http,https,10254
-sudo iptables-legacy -t nat -D CNI-HOSTPORT-DNAT 1
+#2    CNI-DN-6832ce82caf043689e15f  tcp  --  anywhere             anywhere             /* dnat name: "k8s-pod-network" id: "9b452011f0c5049380e9bc4ea23014fb984b8f8e20415f8f9e0c8e1ce5e26ac9" */ multiport dports http,https,10254
+
+echo "Checking iptables: there should be only one rule"
+sudo iptables-legacy -t nat -L CNI-HOSTPORT-DNAT
+
+echo "Trying to remove extra entry"
+sudo iptables-legacy -t nat -D CNI-HOSTPORT-DNAT 2 || echo "No extra entry"
