2d6baa32d020e2b70b2411c432e1b5b1bb6b9305
Sebastien Georget <sebastien.georget@canonical.com>
2025-08-07 10:59:40 +0200

    Fix comment

--- tests/integration/pre_run_script.sh
+++ tests/integration/pre_run_script.sh
@@ -12,7 +12,7 @@
 # Affected rule looks like:
 # num  target     prot opt source               destination
 #1    CNI-DN-6832ce82caf043689e15f  tcp  --  anywhere             anywhere             /* dnat name: "k8s-pod-network" id: "9b452011f0c5049380e9bc4ea23014fb984b8f8e20415f8f9e0c8e1ce5e26ac9" */ multiport dports http,https,10254
-#2    CNI-DN-6832ce82caf043689e15f  tcp  --  anywhere             anywhere             /* dnat name: "k8s-pod-network" id: "9b452011f0c5049380e9bc4ea23014fb984b8f8e20415f8f9e0c8e1ce5e26ac9" */ multiport dports http,https,10254
+#2    CNI-DN-f659e1bd219a5f3afd378  tcp  --  anywhere             anywhere             /* dnat name: "k8s-pod-network" id: "5b1508a92274579cd23b41ff8d9732583b39a2802395d397cafd5389a29c9359" */ multiport dports http,https,10254
 
 echo "Checking iptables: there should be only one rule"
 sudo iptables-legacy -t nat -L CNI-HOSTPORT-DNAT
